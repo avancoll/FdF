7f39d3016760d881ec1b6292799176d6b77a876e
Antoine Vancollie <avancoll@fu-r6-p8.s19.be>
2022-11-04 18:09:06 +0100

    -m

---
+++ SRCS/fdf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avancoll <avancoll@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 15:20:32 by avancoll          #+#    #+#             */
+/*   Updated: 2022/11/04 16:49:46 by avancoll         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# define X_SIZE 5120 / 2
+# define Y_SIZE 2880 / 2
+# include <fcntl.h> //open
+# include <unistd.h> //close read write
+# include <stdlib.h> //malloc free
+# include <stdio.h> // perror
+# include <string.h> //strerror
+# include <stdlib.h> //exit
+# include <math.h> // maths
+# include <mlx.h> //minilibx
+
+#endif
