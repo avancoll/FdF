f0ec19a955f257334c65b4be460464afdda1dc66
Antoine Vancollie <avancoll@fu-r8-p6.s19.be>
2022-11-21 17:33:21 +0100

    init drawmap

--- SRCS/test_drawmap.c
+++ SRCS/test_drawmap.c
@@ -6,7 +6,7 @@
 /*   By: avancoll <avancoll@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 13:39:10 by avancoll          #+#    #+#             */
-/*   Updated: 2022/11/21 15:54:06 by avancoll         ###   ########.fr       */
+/*   Updated: 2022/11/21 17:20:57 by avancoll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,19 @@ void	draw_map(t_data *data, t_coo *coo)
 {
 	(void)coo;
 	int	i = 0;
-	float	A = 45 * (M_PI / 180);
-	float	B = (sin(30) / cos(45)) * (M_PI / 180);
-	float	C = 0;
-	printf("A = %f, B = %f\n", A, B);
+	float	A = 0;
+	float	B = -35.264 * (M_PI / 180);
+	float	C = -45 * (M_PI / 180);
 	data->x = 0;
 	data->y = 0;
 	while (i < 500)
 	{
 		data->x = i;
 		data->y = 0;
-		data->x = (data->x) * cos(A) - (data->y) * sin(A);
-		data->y = (data->x) * sin(A) + (data->y) * cos(A);
-		data->x = data->x;
-		data->y = data->y * cos(B) - 0 * sin(B);
-		data->x = data->x * cos(C) - 0 * sin(C);
-		data->y = data->y;
+		data->x = data->x * (cos(B) * cos(C)) + data->y * ((sin(A) * sin(B) * cos(C)) - (cos(A) * sin(C)));
+		data->y = data->x * (cos(B) * sin(C)) + data->y * ((sin(A) * sin(B) * sin(C)) - (cos(A) * cos(B)));
+		// data->x = data->x * cos(C) - 0 * sin(C);
+		// data->y = data->y * cos(B) - 0 * sin(B);
 		data->x += SIZE_X /2;
 		data->y += SIZE_Y /2;
 		// printf("x = %d et y = %d\n", data->x, data->y);
@@ -45,12 +42,10 @@ void	draw_map(t_data *data, t_coo *coo)
 	{
 		data->x = 0;
 		data->y = i;
-		data->x = (data->x) * cos(A) - (data->y) * sin(A);
-		data->y = (data->x) * sin(A) + (data->y) * cos(A);
-		data->x = data->x;
-		data->y = data->y * cos(B) - 0 * sin(B);
-		data->x = data->x * cos(C) - 0 * sin(C);
-		data->y = data->y;
+		data->x = data->x * (cos(B) * cos(C)) + data->y * ((sin(A) * sin(B) * cos(C)) - (cos(A) * sin(C)));
+		data->y = data->x * (cos(B) * sin(C)) + data->y * ((sin(A) * sin(B) * sin(C)) - (cos(A) * cos(B)));
+		// data->x = data->x * cos(C) - 0 * sin(C);
+		// data->y = data->y * cos(B) - 0 * sin(B);
 		data->x += SIZE_X /2;
 		data->y += SIZE_Y /2;
 		// printf("x = %d et y = %d\n", data->x, data->y);
@@ -64,12 +59,10 @@ void	draw_map(t_data *data, t_coo *coo)
 	{
 		data->x = i;
 		data->y = 500;
-		data->x = (data->x) * cos(A) - (data->y) * sin(A);
-		data->y = (data->x) * sin(A) + (data->y) * cos(A);
-		data->x = data->x;
-		data->y = data->y * cos(B) - 0 * sin(B);
-		data->x = data->x * cos(C) - 0 * sin(C);
-		data->y = data->y;
+		data->x = data->x * (cos(B) * cos(C)) + data->y * ((sin(A) * sin(B) * cos(C)) - (cos(A) * sin(C)));
+		data->y = data->x * (cos(B) * sin(C)) + data->y * ((sin(A) * sin(B) * sin(C)) - (cos(A) * cos(B)));
+		// data->x = data->x * cos(C) - 0 * sin(C);
+		// data->y = data->y * cos(B) - 0 * sin(B);
 		data->x += SIZE_X /2;
 		data->y += SIZE_Y /2;
 		// printf("x = %d et y = %d\n", data->x, data->y);
@@ -83,12 +76,10 @@ void	draw_map(t_data *data, t_coo *coo)
 	{
 		data->x = 500;
 		data->y = i;
-		data->x = (data->x) * cos(A) - (data->y) * sin(A);
-		data->y = (data->x) * sin(A) + (data->y) * cos(A);
-		data->x = data->x;
-		data->y = data->y * cos(B) - 0 * sin(B);
-		data->x = data->x * cos(C) - 0 * sin(C);
-		data->y = data->y;
+		data->x = data->x * (cos(B) * cos(C)) + data->y * ((sin(A) * sin(B) * cos(C)) - (cos(A) * sin(C)));
+		data->y = data->x * (cos(B) * sin(C)) + data->y * ((sin(A) * sin(B) * sin(C)) - (cos(A) * cos(B)));
+		// data->x = data->x * cos(C) - 0 * sin(C);
+		// data->y = data->y * cos(B) - 0 * sin(B);
 		data->x += SIZE_X /2;
 		data->y += SIZE_Y /2;
 		// printf("x = %d et y = %d\n", data->x, data->y);
@@ -102,12 +93,10 @@ void	draw_map(t_data *data, t_coo *coo)
 	{
 		data->x = i;
 		data->y = 0;
-		data->x = (data->x) * cos(A) - (data->y) * sin(A);
-		data->y = (data->x) * sin(A) + (data->y) * cos(A);
-		data->x = data->x;
-		data->y = data->y * cos(B) - 1000 * sin(B);
-		data->x = data->x * cos(C) - 1000 * sin(C);
-		data->y = data->y;
+		data->x = data->x * (cos(B) * cos(C)) + data->y * ((sin(A) * sin(B) * cos(C)) - (cos(A) * sin(C)));
+		data->y = data->x * (cos(B) * sin(C)) + data->y * ((sin(A) * sin(B) * sin(C)) - (cos(A) * cos(B)));
+		// data->y = data->y * cos(B) - 1000 * sin(B);
+		// data->x = data->x * cos(C) - 1000 * sin(C);
 		data->x += SIZE_X /2;
 		data->y += SIZE_Y /2;
 		// printf("x = %d et y = %d\n", data->x, data->y);
@@ -121,12 +110,10 @@ void	draw_map(t_data *data, t_coo *coo)
 	{
 		data->x = 0;
 		data->y = i;
-		data->x = (data->x) * cos(A) - (data->y) * sin(A);
-		data->y = (data->x) * sin(A) + (data->y) * cos(A);
-		data->x = data->x;
-		data->y = data->y * cos(B) - 1000 * sin(B);
-		data->x = data->x * cos(C) - 1000 * sin(C);
-		data->y = data->y;
+		data->x = data->x * (cos(B) * cos(C)) + data->y * ((sin(A) * sin(B) * cos(C)) - (cos(A) * sin(C)));
+		data->y = data->x * (cos(B) * sin(C)) + data->y * ((sin(A) * sin(B) * sin(C)) - (cos(A) * cos(B)));
+		// data->x = data->x * cos(C) - 1000 * sin(C);
+		// data->y = data->y * cos(B) - 1000 * sin(B);
 		data->x += SIZE_X /2;
 		data->y += SIZE_Y /2;
 		// printf("x = %d et y = %d\n", data->x, data->y);
@@ -140,12 +127,10 @@ void	draw_map(t_data *data, t_coo *coo)
 	{
 		data->x = i;
 		data->y = 500;
-		data->x = (data->x) * cos(A) - (data->y) * sin(A);
-		data->y = (data->x) * sin(A) + (data->y) * cos(A);
-		data->x = data->x;
-		data->y = data->y * cos(B) - 1000 * sin(B);
-		data->x = data->x * cos(C) - 1000 * sin(C);
-		data->y = data->y;
+		data->x = data->x * (cos(B) * cos(C)) + data->y * ((sin(A) * sin(B) * cos(C)) - (cos(A) * sin(C)));
+		data->y = data->x * (cos(B) * sin(C)) + data->y * ((sin(A) * sin(B) * sin(C)) - (cos(A) * cos(B)));
+		// data->x = data->x * cos(C) - 1000 * sin(C);
+		// data->y = data->y * cos(B) - 1000 * sin(B);
 		data->x += SIZE_X /2;
 		data->y += SIZE_Y /2;
 		// printf("x = %d et y = %d\n", data->x, data->y);
@@ -159,12 +144,10 @@ void	draw_map(t_data *data, t_coo *coo)
 	{
 		data->x = 500;
 		data->y = i;
-		data->x = (data->x) * cos(A) - (data->y) * sin(A);
-		data->y = (data->x) * sin(A) + (data->y) * cos(A);
-		data->x = data->x;
-		data->y = data->y * cos(B) - 1000 * sin(B);
-		data->x = data->x * cos(C) - 1000 * sin(C);
-		data->y = data->y;
+		data->x = data->x * (cos(B) * cos(C)) + data->y * ((sin(A) * sin(B) * cos(C)) - (cos(A) * sin(C)));
+		data->y = data->x * (cos(B) * sin(C)) + data->y * ((sin(A) * sin(B) * sin(C)) - (cos(A) * cos(B)));
+		// data->x = data->x * cos(C) - 1000 * sin(C);
+		// data->y = data->y * cos(B) - 1000 * sin(B);
 		data->x += SIZE_X /2;
 		data->y += SIZE_Y /2;
 		// printf("x = %d et y = %d\n", data->x, data->y);
